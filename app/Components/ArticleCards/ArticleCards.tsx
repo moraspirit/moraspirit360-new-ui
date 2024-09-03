da27842ecb20a7a8b84d4b3012f3d8ad5e7e9ad3
ISURU VITHANAGE <38922397+IsuruVithanage@users.noreply.github.com>
2024-09-03 19:08:20 +0530

    Article page

---
+++ app/Components/ArticleCards/ArticleCards.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+
+type EventCardProps = {
+    img: string;
+    date: string;
+    title: string;
+    desc: string;
+};
+
+const ArticleCard = ({ img, date, title, desc }: EventCardProps) => {
+    return (
+        <div className="flex flex-col h-[447px] w-[395px]">
+            <div className="overflow-hidden">
+                <Image
+                    src={img}
+                    alt={desc}
+                    width={300}
+                    height={300}
+                    className="transition-transform duration-500 ease-in-out transform hover:scale-105 hover:shadow-lg"
+                />
+            </div>
+            <div className="mt-4 text-white">
+                <h2 className="text-gray-600">{date}</h2>
+                <h3 className="text-lg font-semibold">{title}</h3>
+                <p className="w-[300px] mt-6">{desc}</p>
+            </div>
+        </div>
+    );
+};
+
+export default ArticleCard;
--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -1,10 +1,83 @@
 import Image from "next/image";
+import ArticleCard from "@/app/Components/ArticleCards/ArticleCards";
 
-export default function Home() {
+export default function EventPage() {
     return (
-        <div>
-            <h1>Articles</h1>
-
+        <div className="bg-black ">
+            <div
+                className="mainSection h-[90vh] w-screen opacity-[25%] "
+                style={{
+                    backgroundImage: "url(/eventPageBg.png)",
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: "center",
+                }}
+            ></div>
+            <div className="textContainer  w-screen h-1/2 absolute z-20 top-1/4 flex flex-col items-center justify-center">
+                <h1 className="text-[138px] text-white">Our Articles</h1>
+                <h2 className="text-[50px] text-white">ELEVATE EVERY OCCASION</h2>
+                <span className="text-[30px] text-[#DD2025]">
+          Where Exceptional Events Come to Life
+        </span>
+            </div>
+            <div className="secondSection bg-black w-full h-screen flex text-white justify-center gap-20 items-center">
+                <div className="image w-[709px] h-[473px] flex justify-center items-center relative p-10">
+                    <Image
+                        src={"/eventPageMainEVent.png"}
+                        alt=""
+                        fill={true}
+                        objectFit="contain"
+                        className=""
+                    />
+                </div>
+                <div className="text w-1/3 flex justify-center flex-col">
+                    <span className="text-gray-600">NOVEMBER 24, 2024</span>
+                    <h2>World of Boxing Insights and Stories</h2>
+                    <p className="mt-5">
+                        Explore the latest in boxing with expert analysis, fighter profiles,
+                        and thrilling match recaps. Stay updated on the sport’s most
+                        exciting developments and stories from the ring
+                    </p>
+                </div>
+            </div>
+            <div className="secondSection  w-full  grid grid-cols-3 grid-rows-2 p-10 m-auto gap-8 ml-10 place-items-center">
+                <ArticleCard
+                    img="/EventCard1.png"
+                    date="NOVEMBER 24, 2024"
+                    title="World of Boxing Insights and Stories"
+                    desc="Explore the latest in boxing with expert analysis, fighter profiles, and thrilling match recaps. Stay updated on the sport’s most exciting developments and stories from the ring"
+                />
+                <ArticleCard
+                    img="/EventCard2.png"
+                    date="NOVEMBER 24, 2024"
+                    title="World of Boxing Insights and Stories"
+                    desc="Explore the latest in boxing with expert analysis, fighter profiles, and thrilling match recaps. Stay updated on the sport’s most exciting developments and stories from the ring"
+                />
+                <ArticleCard
+                    img="/EventCard3.png"
+                    date="NOVEMBER 24, 2024"
+                    title="World of Boxing Insights and Stories"
+                    desc="Explore the latest in boxing with expert analysis, fighter profiles, and thrilling match recaps. Stay updated on the sport’s most exciting developments and stories from the ring"
+                />
+                <ArticleCard
+                    img="/EventCard2.png"
+                    date="NOVEMBER 24, 2024"
+                    title="World of Boxing Insights and Stories"
+                    desc="Explore the latest in boxing with expert analysis, fighter profiles, and thrilling match recaps. Stay updated on the sport’s most exciting developments and stories from the ring"
+                />
+                <ArticleCard
+                    img="/EventCard2.png"
+                    date="NOVEMBER 24, 2024"
+                    title="World of Boxing Insights and Stories"
+                    desc="Explore the latest in boxing with expert analysis, fighter profiles, and thrilling match recaps. Stay updated on the sport’s most exciting developments and stories from the ring"
+                />
+                <ArticleCard
+                    img="/EventCard1.png"
+                    date="NOVEMBER 24, 2024"
+                    title="World of Boxing Insights and Stories"
+                    desc="Explore the latest in boxing with expert analysis, fighter profiles, and thrilling match recaps. Stay updated on the sport’s most exciting developments and stories from the ring"
+                />
+            </div>
         </div>
     );
 }
