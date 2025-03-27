d984f3df19ef01349f146b8d169ce5f44c1691be
seniru-dilmith <dilmithseniru@gmail.com>
2025-03-27 15:13:10 +0530

    content repeat fix

--- app/page.tsx
+++ app/page.tsx
@@ -122,28 +122,6 @@ const Home = () => {
           </button>
         </ScrollUpDiv>
       </section>
-      {/* Second Section */}
-      <section
-        className={`${styles.secondSection} ${styles.scrollUpAnimation} flex flex-col items-center justify-center`}
-      >
-        <ScrollUpDiv>
-          <h1>Bringing Your Vision to Life</h1>
-          <p className="font-thin">
-            MoraSpirit 360 is your all-in-one partner for exceptional event
-            coverage, creative marketing solutions, and innovative web
-            development. From capturing every moment with stunning photography
-            and videography to promoting your event with targeted marketing
-            strategies, we bring your vision to life. Our team of experts
-            combines creativity, technology, and a deep understanding of your
-            needs to deliver results that resonate. Whether it's a corporate
-            event, a sports tournament, or a digital project, MoraSpirit 360 is
-            committed to excellence at every step.
-          </p>
-          <button className={styles.requestButton} onClick={navigateToRequest}>
-            REQUEST
-          </button>
-        </ScrollUpDiv>
-      </section>
 
       {/* third Section */}
       <section className={`${styles.thirdSection} ${styles.scrollUpAnimation}`}>
